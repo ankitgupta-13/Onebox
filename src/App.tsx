82ca3937709fb52bba0b9af0e59538bf10fbbce7
ankitgupta-13 <guptankit0522@gmail.com>
2024-08-14 02:57:12 +0530

    feat: add google login

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,14 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
       </>
     )
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,19 +1,39 @@
-import { login } from "../api/auth.api";
+import { useEffect } from "react";
+import google from "../assets/google.svg";
+import logo from "../assets/logoName.svg";
 
 const Login = () => {
   const handleLogin = async () => {
-    try {
-      const response = await login();
-      console.log(response);
-    } catch (error) {}
+    window.location.href = `https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=${window.location.origin}`;
   };
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(window.location.search);
+    const fetchedToken = queryParams.get("token");
+
+    if (fetchedToken) {
+      localStorage.setItem("token", fetchedToken);
+    }
+  }, []);
+
   return (
-    <div className="flex justify-center items-center">
-      <div className="bg-gray-700">
-        <div>Create new account</div>
-        <div>Google</div>
-        <button>Create an Account</button>
-        <p onClick={handleLogin}>Already have an account? Sign in</p>
+    <div className="h-screen">
+      <div className="flex justify-center p-6">
+        <img src={logo} alt="not found" className="h-10" />
+      </div>
+      <div className="flex justify-center items-center h-5/6">
+        <div className="bg-gray-700 flex flex-col items-center gap-4 px-10 py-6">
+          <div className="">Create new account</div>
+          <div className="flex gap-3 cursor-pointer border-2 py-2 px-6 rounded-md">
+            <img src={google} alt="not found" />
+            <span onClick={handleLogin}>Sign Up with Google</span>
+          </div>
+          <button>Create an Account</button>
+          <div className="flex gap-2">
+            <p>Already have an account?</p>
+            <span>Sign in</span>
+          </div>
+        </div>
       </div>
     </div>
   );
